bf9ce2b24a981c8c35c22f44dc34647b8eb2a488
J_Kiser <jdkiser228@gmail.com>
2021-07-12 12:38:26 -0400

    updating components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
-
+import PageHeader from './components/PageHeader/PageHeader';
 import Home from './pages/Home'
 function App() {
   return (
     <div className="App">
+      <PageHeader></PageHeader>
       <Home></Home>
     </div>
   );
